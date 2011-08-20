805edc7541f7125b6a14a015f0002b9c01725eb5
Fredy Sierra <fredysierra@gmail.com>
2011-08-20 16:58:27 -0500

    Finish admin products

--- PeoplePass/src/com/pp/admin/facade/ProductAdmin.java
+++ PeoplePass/src/com/pp/admin/facade/ProductAdmin.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 
 import com.pp.admin.hibernate.KProductos;
 
+
+@Repository
+@Transactional
 public class ProductAdmin implements IProductAdmin {
 
 	@Autowired
